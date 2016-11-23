a1aee5b2e4d66d1779d1fc88ddb15c062b1dcd37
Svetlozar Georgiev <37ernity@gmail.com>
2016-11-23 17:48:36 +0000

    added database + database connection

---
+++ assessment2-cs/AddBookingWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+
+namespace cw2_csharp
+{
+    /// <summary>
+    /// Interaction logic for AddBookingWindow.xaml
+    /// </summary>
+    public partial class AddBookingWindow : Window
+    {
+        public AddBookingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Svetlozar Georgiev\Desktop\assessment2-cs\database\DB.mdf;Integrated Security=True;Connect Timeout=30");
+            string sqlQuery = "INSERT INTO customer VALUES (4, @name, @address)";
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+            cmd.Parameters.AddWithValue("name", txtbx_name.Text);
+            cmd.Parameters.AddWithValue("address", txtbx_address.Text);           
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show(i.ToString() + "rows modified");
+            this.Close();
+        }
+    }
+}
---
+++ assessment2-cs/CustomerSingleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw2_csharp
+{
+    class CustomerSingleton
+    {
+        private static CustomerSingleton _instance;
+
+        private CustomerSingleton() { }
+
+        private String name;
+        private String address;
+        private int refnumber;
+
+        private static CustomerSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new CustomerSingleton();
+                }
+
+                return _instance;
+            }
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public String Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public int Refnumber
+        {
+            get { return refnumber; }
+            set { refnumber = value; }
+        }
+    }
+}
